a50c3e7184922e433fa55c35ac61e1f4f5bdde93
Sutarodo <joshuacrowe@outlook.com>
2020-03-21 23:38:42 +0000

    Converted AuditList to Custom CSS

--- src/Components/AuditList/AuditList.jsx
+++ src/Components/AuditList/AuditList.jsx
@@ -2,56 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import ReactList from "react-list";
-import { makeStyles } from "@material-ui/styles"
-import { Scrollbars } from "react-custom-scrollbars";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import Paper from "@material-ui/core/Paper";
-import Divider from "@material-ui/core/Divider";
+import CloseIcon from "@material-ui/icons/Close"
 import Audit from "../Audit/Audit";
 import AuditShape from "../../Shapes/AuditShape"
 import ClearAudits from "../../Store/Audit/AuditActions/ClearAudits"
+import "./auditList.scss";
 
-const useStyles = makeStyles({
-    root: {
-        height: "40vh"
-    }
-})
-
-const AuditList = ({ audits, clearTheseAudits }) => {
-    const classes = useStyles();
-    return (
-        <>
-            <ListSubheader>
-                <span>
-                    Your Audits ({audits.length})
-                    {audits.length > 250 && <strong> -  Warning: High Memory Usage</strong>}
-                </span>
-                <ListItemSecondaryAction>
-                    <IconButton
-                        edge="end"
-                        onClick={clearTheseAudits}
-
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListSubheader>
-            <Divider />
-            <Paper elevation={5} className={classes.root}>
-                <Scrollbars autoHide>
-                    <ReactList
-                        itemRenderer={(index, key) => <Audit key={key} audit={audits[index]} />}
-                        length={audits.length}
-                        type="uniform"
-                    />
-                </Scrollbars>
-            </Paper>
-        </>
-    );
-}
+const AuditList = ({ audits, clearTheseAudits }) => (
+    <div className="auditList">
+        <div className="auditList-header">
+            <div className="auditList-header-left">
+                <p>Your Audits <span className="auditList-count">({audits.length})</span></p>
+            </div>
+            <div className="auditList-header-right">
+                <button onClick={clearTheseAudits}>
+                    <CloseIcon />
+                </button>
+            </div>
+        </div>
+        <div className="auditList-container">
+            <ReactList
+                itemRenderer={(index, key) => <Audit key={key} audit={audits[index]} />}
+                length={audits.length}
+                type="uniform"
+            />
+        </div>
+    </div>
+);
 
 AuditList.propTypes = {
     actions: PropTypes.arrayOf(AuditShape),
